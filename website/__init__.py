674afb22326da7cffc069d9d5a79408fe0765b6a
NIK-llama <nikshitog@gmail.com>
2025-08-13 20:10:29 +0530

    major commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,37 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default-key')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from . import models
+
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    db_path = os.path.join(os.path.dirname(__file__), DB_NAME)
+    if not os.path.exists(db_path):
+        with app.app_context():
+            db.create_all()
+        print("Created Database!")
+
